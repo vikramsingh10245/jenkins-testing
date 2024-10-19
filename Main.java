708eea96323ac3dec892d43307a1af3c0f3025dd
vikramsingh10245 <60178700+vikramsingh10245@users.noreply.github.com>
2024-10-19 16:33:21 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main{
 	public static void main (String[] args){
 		System.out.println("this is java program");
 System.out.println("This is testing for Jenkins program");
+		System.out.println("This is new Line");
 
 }
 }
